bb251edbbba46832179925f32a7619b99c2e952a
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-04-15 18:00:00 -0300

    Create attendeeDTO

---
+++ src/main/java/techsolucoes/com/passin/dto/attendee/AttendeeDetails.java
@@ -0,0 +1,7 @@
+package techsolucoes.com.passin.dto.attendee;
+
+import java.time.LocalDateTime;
+
+public record AttendeeDetails(String id, String name, String email, LocalDateTime createdAt,
+                              LocalDateTime checkedInAt) {
+}
---
+++ src/main/java/techsolucoes/com/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -0,0 +1,7 @@
+package techsolucoes.com.passin.dto.attendee;
+
+
+import java.util.List;
+
+public record AttendeesListResponseDTO(List<AttendeeDetails> attendees) {
+}
