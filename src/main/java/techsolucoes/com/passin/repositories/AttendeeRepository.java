5c160f7574f2508aca52e7749eeb0505a49965d6
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-04-15 14:57:59 -0300

    Repository creation

--- src/main/java/techsolucoes/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/techsolucoes/com/passin/repositories/AttendeeRepository.java
@@ -3,5 +3,10 @@ package techsolucoes.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import techsolucoes.com.passin.domain.attendee.Attendee;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+
+    List<Attendee> findByEventId(String eventId);
+
 }
