b624d8974f123cff3904fc98878a9d68dc678cab
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-04-15 15:54:16 -0300

    Update DTO

--- src/main/java/techsolucoes/com/passin/dto/event/EventResponseDTO.java
+++ src/main/java/techsolucoes/com/passin/dto/event/EventResponseDTO.java
@@ -1,7 +1,12 @@
 package techsolucoes.com.passin.dto.event;
 
+import lombok.Getter;
+import lombok.Setter;
 import techsolucoes.com.passin.domain.event.Event;
 
+@Getter
+
+//Classe usada para retornar os dados do evento
 public class EventResponseDTO {
     EventDetailDTO event;
 
