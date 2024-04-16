72f0f356bf5dd9f2afc809ab5dd1b1567624e68a
iFallenHunt <lukas_santos.silva@hotmail.com>
2024-04-16 17:51:44 -0300

    Error configuration class

--- src/main/java/techsolucoes/com/passin/config/ExceptionEntityHandler.java
+++ src/main/java/techsolucoes/com/passin/config/ExceptionEntityHandler.java
@@ -1,9 +1,15 @@
 package techsolucoes.com.passin.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import techsolucoes.com.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
+import techsolucoes.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import techsolucoes.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import techsolucoes.com.passin.domain.event.exceptions.EventFullException;
 import techsolucoes.com.passin.domain.event.exceptions.EventNotFoundException;
+import techsolucoes.com.passin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
@@ -11,4 +17,24 @@ public class ExceptionEntityHandler {
     public ResponseEntity handleEventNotFound(EventNotFoundException e) {
         return ResponseEntity.notFound().build();
     }
+    @ExceptionHandler(EventFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventFullNotFound(EventFullException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity handleAttendeeNotFound(AttendeeNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyExistException.class)
+    public ResponseEntity handleAttendeeAlreadyExistNotFound(AttendeeAlreadyExistException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity handleCheckInAlreadyExistNotFound(CheckInAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
 }
